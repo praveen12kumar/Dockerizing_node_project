4b2f1b3422bf633f7fcee80720f2c6a0d5e6f62d
Praveen Kumar <praveenshakya441@gmail.com>
2025-06-20 11:26:32 +0530

    Added .env

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
+dotenv.config();
 
 const app = express();
 
@@ -10,6 +12,6 @@ app.get('/home', (req, res) => {
     });
 })
 
-app.listen(3000, '0.0.0.0', () => {
-  console.log('Server is running on http://localhost:3000');
+app.listen(process.env.PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port: ${process.env.PORT}`);
 });
